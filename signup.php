62c2d2b5138d17da4201a81bb6cebb030b6d0964
shikher7 <shikher707@gmail.com>
2017-09-28 03:32:23 +0530

    Update Singup Page

--- signup.php
+++ signup.php
@@ -1,6 +1,45 @@
 <html>
 <head>
-
+    <style>
+        input[type= "text"], input[type="password"]{
+            outline: none;
+            padding:10px;
+            display: block;
+            width: 300px;
+            border-radius: 3px;
+            border: 1px solid #993300;
+            margin-top: 10px;
+        }
+        input[type="submit"]{
+            padding: 10px;
+            width: 320px;
+            color:#fff;
+            background: #0098cb;
+            margin: 20px auto;
+            margin-top: 10px;
+            cursor: pointer;
+            border-radius: 3px;
+            border:0;
+        }
+        input[type="submit"]:hover{
+            background:#00b8eb;
+        }
+        select{
+            padding: 10px;
+            width: 320px;
+            color:#fff;
+            background: rgba(87,87,85,0.87);
+            margin-top: 10px;
+            cursor: pointer;
+            border-radius: 3px;
+            border:0;
+        }
+        textarea
+        {
+            margin-top: 10px;
+        }
+    </style>
+</head>
 <title>New user signup </title>
 <script language="javascript">
 function check()
@@ -67,13 +106,14 @@ function check()
 <?php
 include("header.php");
 ?>
- <table width="100%" border="0">
+<br><br><div style='margin:auto; width:60%; text-align:center;box-shadow:2px 3px 2px 3px #CCCCCC; border-radius:10px ;text-align:left; padding:3%'>
+ <table width="100%" border="0" style="margin-left: 12.5%">
    <tr>
      <td width="132" rowspan="2" valign="top"><span class="style8"><img src="images/connected_multiple_big.jpg" width="131" height="155"></span></td>
-     <td width="468" height="57"><h1 align="center"><span class="style8">New User Signup</span></h1></td>
+     <td width="468" height="57"><h1 style="margin-left: 12.5%"><span class="style5">New User Signup</span></h1></td>
    </tr>
    <tr>
-     <td><form name="form1" method="post" action="signupuser.php" onSubmit="return check();">
+     <td><div name="form1" method="post" action="signupuser.php" onSubmit="return check();">
        <table width="301" border="0" align="left">
          <tr>
            <td><div align="left" class="style7">Login Id </div></td>
@@ -100,10 +140,19 @@ include("header.php");
            <td><input type="submit" name="Submit" value="Signup">
            </td>
          </tr>
+           <tr>
+               <td>&nbsp;</td>
+               <td>Already have a account <a href="index.php">SIGN IN</a>
+               </td>
+           </tr>
        </table>
-     </form></td>
+
+             </form>
+         </div></td>
    </tr>
+
  </table>
+</div>
  <p>&nbsp; </p>
 </body>
 </html>
