8673f81d76181525515cf404cd538b28c1d3ab54
shikher7 <shikher707@gmail.com>
2017-10-30 04:44:34 +0530

    Update Ranking UI ( Update database before pull )

--- ranking.php
+++ ranking.php
@@ -13,23 +13,43 @@ session_start();
 include("header.php");
 include("database.php");
 extract($_SESSION);
-$rs=mysql_query("select login, SUM(F.score) AS TOTAL from (SELECT t.login,t.test_id,t.score FROM `mst_result` t WHERE t.score=(SELECT MAX(a.score) FROM `mst_result` a WHERE a.test_id=t.test_id))AS F group by login ORDER BY SUM(F.score) DESC",$cn) or die(mysql_error());
+echo "<h1 class=head2 style='text-align:center;'> Ranking </h1>";
+$r1=mysql_query("select * from mst_subject");
 
-echo "<h1 class=head1> Ranking </h1>";
-if(mysql_num_rows($rs)<1)
-{
-    echo "<br><h1 class=head1> No one has given any quiz</h1>";
-    exit;
+while($row1=mysql_fetch_row($r1)) {
+    $z=0;
+
+
+    $sub_id=$row1[0];
+    echo "<div style='padding:20px; margin:auto; width:40%; text-align:center;box-shadow:2px 3px 2px 3px #CCCCCC; border-radius:10px ;text-align:left'>";
+    echo "<h1 class=head1> $row1[1]</h1><hr><hr><br><br>";
+    $rs = mysql_query("select login, SUM(F.score) AS TOTAL from (SELECT t.login,t.test_id,t.score FROM `mst_result` t WHERE t.score=(SELECT MAX(a.score) FROM `mst_result` a WHERE a.test_id=t.test_id ) and t.sub_id=$sub_id)AS F group by login ORDER BY SUM(F.score) DESC", $cn) or die(mysql_error());
+
+
+
+    if (mysql_num_rows($rs) < 1) {
+        echo "<br><h3 style='text-align: center'> No attempts made by any user..!!</h1>";
+        $z=1;
+        cotinue;
+    }
+
+    $i = 1;
+    if($z!=1)
+    echo "<table align=center><tr class=style2><td width=300>User Name<hr> <td> Total Score<hr> <td> Rank<hr>";
+    while ($row = mysql_fetch_row($rs)) {
+
+
+        echo "<tr class=style8><td>$row[0] <td align=center> $row[1] <td align=center> $i";
+
+        $i++;
+
+    }
+    echo "</table>";
+    echo "<br><br>";
+    echo "</div>";
+    echo "<br><br>";
 }
-echo "<div style='padding:20px; margin:auto; width:40%; text-align:center;box-shadow:2px 3px 2px 3px #CCCCCC; border-radius:10px ;text-align:left'>";
-echo "<table align=center><tr class=style2><td width=300>User Name<hr> <td> Total Score<hr> <td> Rank<hr>";
-$i=1;
-while($row=mysql_fetch_row($rs))
-{
-    echo "<tr class=style8><td>$row[0] <td align=center> $row[1] <td align=center> $i";
-    $i++;
-}
-echo "</table></div>";
 ?>
+
 </body>
 </html>
